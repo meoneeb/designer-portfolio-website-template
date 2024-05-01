825e1709d2408d7b4b4b3fe0905ae3f7a5f38bb0
Oneeb Faisal <oneebfaisal@gmail.com>
2024-05-01 23:09:03 +0000

    edits

--- src/components/portfolio/Sidebar/CountAuthorPosts.js
+++
@@ -1,9 +0,0 @@
-import { portfolioArr } from "@/database/portfolio"; // Import the portfolioArr array
-
-export default function CountAuthorPosts(author) {
-  // Filter the portfolioArr to find objects with the same Author value
-  const objectsWithSameAuthor = portfolioArr.filter(item => item.Author.Name === author);
-  
-  // Return the count of objects with the same Author value
-  return objectsWithSameAuthor.length;
-}
